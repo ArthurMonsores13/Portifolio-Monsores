adf92c20aeceef5bf935935f41dda42e3df975af
ArthurMonsores13 <angelaeartur@gmail.com>
2025-05-07 20:55:27 -0300

    teste

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("contactForm");
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      alert("Mensagem enviada! Entraremos em contato em breve.Obrigado!"); /*Exibira uma mensagem */
-      form.reset();
-    });
-  });
